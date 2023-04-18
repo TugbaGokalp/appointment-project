76bc137a33fa408057a61c8e73bc98465a1ab671
tugba-dotcom <tugbagokalp7265@gmail.com>
2023-04-18 12:59:59 +0300

    styling

--- src/components/Doctors.jsx
+++ src/components/Doctors.jsx
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 
 const Doctors = ({ appointments, doctors, setAppointments }) => {
-  return (
-    <Container>
-      <h3>Our Doctors</h3>
 
-      <Row className="gap-2 rounded-2">
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <Container className="p-2">
+      <h3 className="display-6 mb-3" style={{ color: "rgb(166, 18, 189)" }}>Our Doctors</h3>
+      <hr />
+
+      <Row className="justify-content-center" >
         {doctors.map((doctors) => {
           return (
-            <Col>
-              <img className="img-thumbnail doctor-img" src={doctors.img} alt={doctors.img} />
-              <h5>{doctors.name}</h5>
+            <Col className="mt-3" key={doctors.id} xs={6} sm={4} md={3}>
+              <img className="img-thumbnail doctor-img" src={doctors.img} alt={doctors.img} onClick={handleShow} />
+              <h5 className="mt-2">{doctors.name}</h5>
               <h6>{doctors.dep}</h6>
             </Col>
           );
