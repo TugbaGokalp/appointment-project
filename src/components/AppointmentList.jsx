a7123b482c8883d19cef192b74921c733bb1a851
tugba-dotcom <tugbagokalp7265@gmail.com>
2023-04-19 23:56:24 +0300

    AppointmentList Structure

--- src/components/AppointmentList.jsx
+++ src/components/AppointmentList.jsx
@@ -1,14 +1,43 @@
-import React from 'react'
-import { Container } from 'react-bootstrap'
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { FaTimesCircle } from "react-icons/fa"
 
-const AppointmentList = ({appointments}) => {
+
+const AppointmentList = ({ appointments, setAppointments }) => {
+
+  const handleDelete = () => {
+    setAppointments()
+  }
   return (
-    <Container className='p-2'>
-      <h3 className='display-6 mb-2'>Appointment List</h3>
-      {!appointments.length && <img src='./img/appointment.jpg' width="50%" alt='appointment-img'/>}
-        
-    </Container>
-  )
-}
+    <Container className="p-2">
+      <h3 className="display-6 mb-2">Appointment List</h3>
+      {!appointments.length && (
+        <img src="./img/appointment.jpg" width="50%" alt="appointment-img" />
+      )}
 
-export default AppointmentList
+      {appointments.map((patient, doctor, id, day, consulted) => {
+        <div>
+          <Row className="justify-content-center align-items-center g-3">
+            <Col xs={12} sm={12} md={6}>
+              <h4 className="text-danger">{patient}</h4>
+              <h5>{doctor}</h5>
+            </Col>
+            <Col>
+              <h5>Date:{new Date(day).toLocaleDateString()}</h5>
+              <h6>Time:{new Date(day).toLocaleDateString()}</h6>
+            </Col>
+            <Col xs={2} sm={4} md={1} className="text-end">
+              <FaTimesCircle
+                className="text-danger fs-1"
+                type="button"
+                onClick={() => handleDelete(id)}
+              />
+            </Col>
+          </Row>
+        </div>;
+      })}
+    </Container>
+  );
+};
+
+export default AppointmentList;
