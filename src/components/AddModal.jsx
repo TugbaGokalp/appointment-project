b3a535b17b6c3d022438867b11202be3c3ee6c15
tugba-dotcom <tugbagokalp7265@gmail.com>
2023-04-19 22:34:34 +0300

    handleSubmit func added

--- src/components/AddModal.jsx
+++ src/components/AddModal.jsx
@@ -3,7 +3,28 @@ import { Form } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 
-function AddModal({ show, handleClose, drName }) {
+function AddModal({ show, handleClose, drName, appointments, setappointments }) {
+
+  const [patientName, setPatientName] = useState("")
+  const [date, setDate] = useState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setappointments([
+      ...appointments,
+      {
+        id: appointments.length + 1,
+        patient: patientName,
+        day: new Date(),
+        consulted: false,
+        doctor: drName,
+      },
+    ])
+    handleClose()
+
+  }
+
+  console.log(patientName, date)
   return (
     <>
       <Modal show={show} handleClose={handleClose}>
@@ -12,12 +33,13 @@ function AddModal({ show, handleClose, drName }) {
         </Modal.Header>
         <Modal.Body>
         
-        <Form >
+        <Form onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="name">
               <Form.Label>Patient Name</Form.Label>
               <Form.Control
                 type="text"
                 placeholder="Enter your name"
+                onChange={(e) => {setPatientName(e.target.value)}}
                 
               />
             </Form.Group>
@@ -26,7 +48,7 @@ function AddModal({ show, handleClose, drName }) {
               <Form.Label>Day&Time</Form.Label>
               <Form.Control
                 type="datetime-local"
-                
+                onChange={(e) => {setDate(e.target.value)}}
               />
             </Form.Group>
 
@@ -35,7 +57,7 @@ function AddModal({ show, handleClose, drName }) {
 
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="primary">Submit</Button>
+          <Button type="submit" variant="primary">Submit</Button>
           <Button onClick={handleClose} variant="danger">Close</Button>
         </Modal.Footer>
       </Modal>
--- src/components/Doctors.jsx
+++ src/components/Doctors.jsx
@@ -3,28 +3,36 @@ import { Col, Container, Row } from "react-bootstrap";
 import AddModal from "./AddModal";
 
 const Doctors = ({ appointments, doctors, setAppointments }) => {
-
   const [show, setShow] = useState(false);
-  const [selectedDrName, setSelectedDrName] = useState("")
+  const [selectedDrName, setSelectedDrName] = useState("");
 
   const handleClose = () => setShow(false);
   // const handleShow = () => setShow(true);
 
   const handleClick = (drName) => {
-    setShow(true)
-    setSelectedDrName(drName)
-  }
+    setShow(true);
+    setSelectedDrName(drName);
+  };
 
   return (
     <Container className="p-2">
-      <h3 className="display-6 mb-3" style={{ color: "rgb(166, 18, 189)" }}>Our Doctors</h3>
+      <h3 className="display-6 mb-3" style={{ color: "rgb(166, 18, 189)" }}>
+        Our Doctors
+      </h3>
       <hr />
 
-      <Row className="justify-content-center" >
+      <Row className="justify-content-center">
         {doctors.map((doctors) => {
           return (
             <Col className="mt-3" key={doctors.id} xs={6} sm={4} md={3}>
-              <img className="img-thumbnail doctor-img" src={doctors.img} alt={doctors.img} onClick={() => {handleClick(doctors.name)}} />
+              <img
+                className="img-thumbnail doctor-img"
+                src={doctors.img}
+                alt={doctors.img}
+                onClick={() => {
+                  handleClick(doctors.name);
+                }}
+              />
               <h5 className="mt-2">{doctors.name}</h5>
               <h6>{doctors.dep}</h6>
             </Col>
@@ -32,7 +40,13 @@ const Doctors = ({ appointments, doctors, setAppointments }) => {
         })}
       </Row>
 
-      <AddModal show={show} handleClose={handleClose} drName={selectedDrName}/>
+      <AddModal
+        show={show}
+        handleClose={handleClose}
+        drName={selectedDrName}
+        appointments={appointments}
+        setAppointments={setAppointments}
+      />
     </Container>
   );
 };
